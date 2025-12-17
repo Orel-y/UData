e6aa83cd0e2b606ed9c5af1b06c73b5adfa53169
Yoseph won <yosephwondimu26@gmail.com>
2025-12-17 22:18:41 +0300

    Configure CORS policy

--- udatabackend/main.py
+++ udatabackend/main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from app.core.database import create_tables
 from app.core.database import BaseModel, engine
 from app.routers import campuses, buildings, rooms
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="UData")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 def startup():
@@ -16,3 +32,4 @@ def home():
 app.include_router(campuses.router)
 app.include_router(buildings.router)
 app.include_router(rooms.router)
+
