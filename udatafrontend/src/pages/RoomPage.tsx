9fc4a38e74bb01aa4e33c5716116fa778eccf122
Yoseph won <yosephwondimu26@gmail.com>
2025-12-17 13:27:19 +0300

    Fix padding issue

--- udatafrontend/src/pages/RoomPage.tsx
+++ udatafrontend/src/pages/RoomPage.tsx
@@ -44,7 +44,7 @@ export default function RoomPage({
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-6">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 space-y-6">
         <div className="flex items-center justify-between mb-6">
             <div>
                 <h1 className="text-gray-900 text-2xl font-bold">{building.name}</h1>
